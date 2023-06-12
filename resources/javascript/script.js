0af57a7a1e2784e64a307392afb98daaac1f3ac3
Bruceiplol <bruceihizzz@gmail.com>
2023-06-12 23:45:26 +0100

    update

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -12,3 +12,25 @@ hamburger.addEventListener('click', function() {
       menu.style.width = '100%';
     }
   });
+
+
+window.addEventListener('scroll', function() {
+    let scrollPosition = document.documentElement.scrollTop;
+    let scrollToTopButton = document.querySelector('.scroll-to-top');
+  
+    if (scrollPosition > 300) {
+      scrollToTopButton.classList.add('show');
+    } else {
+      scrollToTopButton.classList.remove('show');
+    }
+});
+  
+document.querySelector('.scroll-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+});
+  
+  
